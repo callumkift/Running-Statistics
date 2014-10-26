9e477d50e7ea9c62616956a26268a9b9c2acdf79
Callum Kift <callumkift@gmail.com>
2014-10-26 18:01:13 +0100

    Created script for to calculate stats, welcome message

---
+++ running_data.py
@@ -0,0 +1,11 @@
+##########
+#
+# This script was created so that a runner can gain
+# more insights into their runs. 
+#
+# Creator: Callum Kift
+# email: callumkift@gmail.com
+#
+##########
+
+print "Welcome to your running statistics"
