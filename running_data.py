195aeda5c07b452a32fa18929c49b6f13df0d36b
Callum Kift <callumkift@gmail.com>
2014-10-26 19:06:41 +0100

    Writes run info to file

--- running_data.py
+++ running_data.py
@@ -9,15 +9,32 @@
 ##########
 import os.path
 
-def getfile():
+def GetFile():
 	### Get's file to add runs to.
 	if (os.path.isfile("past_runs.txt")):
 		print "file exists"
-		f = file("past_runs.txt", "a") # opens and appends to file
+		#f = open("past_runs.txt", "a") # opens and appends to file
+		RunInfo("previous")
 	else:
 		# Creates file
 		print "This seems to be your first run - congratulations."
-		f = file("past_runs.txt", "w")
+		#f = open("past_runs.txt", "w")
+		RunInfo("new")
+	return
+
+def RunInfo(a):
+	### User inputs data and it writes to file
+	run_date = raw_input("What was the date of the run? (YYYY/MM/DD)\n")
+	run_distance = raw_input("How far did you run? (x.yz km)\n")
+	run_time = raw_input("How long did you run for? (HH:MM:SS)\n") 
+
+	if (a == "previous"):
+		f = open("past_runs.txt", "a") # opens and appends to file
+	elif (a == "new"):
+		f = open("past_runs.txt", "w")
+
+	f.write(run_date + " " + run_distance + " " + run_time + "\n") 
+	f.close
 	return
 
 print "----------------------------------"
@@ -28,7 +45,7 @@ add_run = raw_input("Do you want to add a new run? (y/n) \npress 'n' if you want
 
 if (add_run == "y" or add_run == "Y"):
 	print "yes detected"
-	getfile()
+	GetFile()
 	# Check to see if there is a file
 	# If not create it
 	# Input data
